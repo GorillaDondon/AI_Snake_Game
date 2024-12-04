b50180b2a3012b018a013e43d616bb1db6d4ab30
ukaleel <125523274+ukaleel@users.noreply.github.com>
2024-12-04 00:19:09 -0600

    older version w/o updated fitness for presentation

--- DataOutput.py
+++ DataOutput.py
@@ -6,13 +6,13 @@ import numpy as np
 import os
 
 # Snake game settings
-snake_speed = 500  # Speed of the snake
-window_x = 720
-window_y = 480
-population_size = 50  # Number of neural networks in each generation
+snake_speed = 1000  # Speed of the snake
+window_x = 500
+window_y = 500
+population_size = 200  # Number of neural networks in each generation
 initial_mutation_rate = 0.1  # Initial probability of mutation
-num_generations = 50  # Number of generations
-visualize_count = 3  # Number of neural networks to visualize from each generation
+num_generations = 1500  # Number of generations
+visualize_count = 1  # Number of neural networks to visualize from each generation
 
 
 black = pygame.Color(0, 0, 0)
@@ -20,6 +20,7 @@ white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
+
 
 pygame.init()
 
@@ -29,15 +30,11 @@ game_window = pygame.display.set_mode((window_x, window_y))
 fps = pygame.time.Clock()
 
 
-# Simple random initialization for neural network weights
-def random_initialization(layer_size, prev_layer_size):
-    return np.random.randn(layer_size, prev_layer_size) * 0.01  # Small random values
-
 # Neural Network class
 # used for each snake AI generation
 # will need modification for inputs and layers as we progress
 class NeuralNetwork:
-    def __init__(self, input_size=14, hidden_layers=[16, 32, 64, 128, 256], output_size=4):
+    def __init__(self, input_size=20, hidden_layers=[16, 32, 64, 32], output_size=4):
         # Neural Network Structure
         # Input size represents # of inputs given to the neural network using the get_game_state function
         # Hidden layer values represent the number of neurons in a hidden layer, optimizable
@@ -47,25 +44,18 @@ class NeuralNetwork:
         self.weights = []
         self.biases = []
 
+        # Initializes the prev layer to input size as only "1" layer so far
         prev_layer_size = input_size
 
         # Generates the weights for each layer, connecting the neurons from layer to layer
         for hidden_size in hidden_layers:
-            self.weights.append(random_initialization(hidden_size, prev_layer_size))
+            self.weights.append(np.random.rand(hidden_size, prev_layer_size) * 2 - 1)
             self.biases.append(np.zeros((hidden_size, 1)))
             prev_layer_size = hidden_size
 
 
-        self.weights.append(random_initialization(output_size, prev_layer_size))
+        self.weights.append(np.random.rand(output_size, prev_layer_size) * 2 - 1)
         self.biases.append(np.zeros((output_size, 1)))
-
-        # Momentum is used to accelerate the learning process of the model to push model towards optimization
-        # Variance represents how much a model's output can fluctuate, specifically related to
-        # overfitting/underfitting issues
-        self.momentum_weights = [np.zeros_like(w) for w in self.weights]
-        self.variance_weights = [np.zeros_like(w) for w in self.weights]
-        self.momentum_biases = [np.zeros_like(b) for b in self.biases]
-        self.variance_biases = [np.zeros_like(b) for b in self.biases]
 
     # Relu and Sigmoid functions used to squash outputs
     def relu(self, x):
@@ -94,26 +84,41 @@ class NeuralNetwork:
 
     # Crossbreeds 2 neural networks to create a child neural network
     def crossover(self, other):
-        child = NeuralNetwork(input_size=len(self.weights[0][0]), hidden_layers=[layer.shape[0] for layer in self.weights[:-1]], output_size=self.weights[-1].shape[0])
+        child = NeuralNetwork(
+            input_size=len(self.weights[0][0]),
+            hidden_layers=[layer.shape[0] for layer in self.weights[:-1]],
+            output_size=self.weights[-1].shape[0]
+        )
+
         for i in range(len(self.weights)):
-            child.weights[i] = np.where(np.random.rand(*self.weights[i].shape) > 0.5, self.weights[i], other.weights[i])
-            child.biases[i] = np.where(np.random.rand(*self.biases[i].shape) > 0.5, self.biases[i], other.biases[i])
+            random_weights = np.random.rand(*self.weights[i].shape)
+            child.weights[i] = np.where(random_weights > 0.5, self.weights[i], other.weights[i])
+
+            random_biases = np.random.rand(*self.biases[i].shape)
+            child.biases[i] = np.where(random_biases > 0.5, self.biases[i], other.biases[i])
+           # child.weights[i] = np.where(np.random.rand(*self.weights[i].shape) > 0.5, self.weights[i],
+            # other.weights[i])
+            #child.biases[i] = np.where(np.random.rand(*self.biases[i].shape) > 0.5, self.biases[i], other.biases[i])
         return child
 
     # Mutates the neural network biases/weights under a certain condition
     def mutate(self, mutation_rate):
         for i in range(len(self.weights)):
-            mutation_mask_w = np.random.rand(*self.weights[i].shape) < mutation_rate
-            mutation_mask_b = np.random.rand(*self.biases[i].shape) < mutation_rate
-            self.weights[i] += mutation_mask_w * np.random.randn(*self.weights[i].shape)
-            self.biases[i] += mutation_mask_b * np.random.randn(*self.biases[i].shape)
+            random_weights = np.random.rand(*self.weights[i].shape)
+            mutation_weight_flag = random_weights < mutation_rate
+
+            random_biases = np.random.rand(*self.biases[i].shape)
+            mutation_bias_flag =  random_biases < mutation_rate
+
+            self.weights[i] += mutation_weight_flag * np.random.randn(*self.weights[i].shape)
+            self.biases[i] += mutation_bias_flag * np.random.randn(*self.biases[i].shape)
 
 # Gives the relevant game state information to the neural network
 # Which then allows it to make predictions
 # Modifying as needed
 
 def get_game_state(snake_position, fruit_position, snake_body, direction):
-    # Relative position of the fruit to the snake
+    #Relative position of the fruit to the snake
     relative_fruit_x = (fruit_position[0] - snake_position[0]) / window_x
     relative_fruit_y = (fruit_position[1] - snake_position[1]) / window_y
 
@@ -135,23 +140,49 @@ def get_game_state(snake_position, fruit_position, snake_body, direction):
     danger_up = 1 if (snake_position[0], snake_position[1] - 10) in snake_body or snake_position[1] - 10 < 0 else 0
     danger_down = 1 if (snake_position[0], snake_position[1] + 10) in snake_body or snake_position[1] + 10 >= window_y else 0
 
+    # Tail position relative to the snake's head
+    if len(snake_body) > 1:
+        tail_position = snake_body[-1]
+    else:
+        tail_position = snake_position
+    relative_tail_x = (tail_position[0] - snake_position[0]) / window_x
+    relative_tail_y = (tail_position[1] - snake_position[1]) / window_y
+
+    # Tail direction
+    if len(snake_body) > 1:
+        tail_direction = (snake_body[-2][0] - tail_position[0], snake_body[-2][1] - tail_position[1])
+        tail_up = 1 if tail_direction == (0, -10) else 0
+        tail_down = 1 if tail_direction == (0, 10) else 0
+        tail_left = 1 if tail_direction == (-10, 0) else 0
+        tail_right = 1 if tail_direction == (10, 0) else 0
+    else:
+        tail_up = tail_down = tail_left = tail_right = 0
+
     return [
         relative_fruit_x, relative_fruit_y,
         danger_left, danger_right, danger_up, danger_down,
         direction_up, direction_down, direction_left, direction_right,
-        fruit_direction_up, fruit_direction_down, fruit_direction_left, fruit_direction_right
+        fruit_direction_up, fruit_direction_down, fruit_direction_left, fruit_direction_right,
+        relative_tail_x, relative_tail_y,
+        tail_up, tail_down, tail_left, tail_right
     ]
 
 
 
 
-def initialize_population(population_size):
-    return [NeuralNetwork() for _ in range(population_size)]
+
+def initialize_population(populationSize):
+    listOfNeuralNetworks = []
+    for i in range(population_size):
+        newChild = NeuralNetwork()
+        listOfNeuralNetworks.append(newChild)
+    return listOfNeuralNetworks
 
 
 # Fitness function to determine worth of a neural network
 # Utilizes steps and distance from fruit to update score
-def evaluate_fitness(nn, max_steps=10000, no_progress_steps=1000):
+
+def evaluate_fitness(nn, index, max_steps=5000, no_progress_steps=1000):
     snake_position = [100, 50]
     snake_body = [[100, 50], [90, 50], [80, 50], [70, 50]]
     fruit_position = [random.randrange(1, (window_x // 10)) * 10, random.randrange(1, (window_y // 10)) * 10]
@@ -159,6 +190,7 @@ def evaluate_fitness(nn, max_steps=10000, no_progress_steps=1000):
     direction = 'RIGHT'
     steps = 0
     score = 0
+    fruits_eaten = 0
     last_distance = np.inf
     progress = False
     direction_change_count = 0
@@ -171,24 +203,24 @@ def evaluate_fitness(nn, max_steps=10000, no_progress_steps=1000):
         game_state = get_game_state(snake_position, fruit_position, snake_body, direction)
         # Gives the move that the neural network has decided to do based off game state and weights/biases
         prediction = nn.predict(game_state)
-        change_to = ['UP', 'DOWN', 'LEFT', 'RIGHT'][prediction]
+        direction_change = ['UP', 'DOWN', 'LEFT', 'RIGHT'][prediction]
 
 
         # a counter variable, not necessary
-        if change_to != direction:
+        if direction_change != direction:
             direction_change_count += 1
 
         # Changes the direction if needed based off prediction
-        if change_to == 'UP' and direction != 'DOWN':
+        if direction_change == 'UP' and direction != 'DOWN':
             direction = 'UP'
-        if change_to == 'DOWN' and direction != 'UP':
+        if direction_change == 'DOWN' and direction != 'UP':
             direction = 'DOWN'
-        if change_to == 'LEFT' and direction != 'RIGHT':
+        if direction_change == 'LEFT' and direction != 'RIGHT':
             direction = 'LEFT'
-        if change_to == 'RIGHT' and direction != 'LEFT':
+        if direction_change == 'RIGHT' and direction != 'LEFT':
             direction = 'RIGHT'
 
-        #Changes position of snake based off direcction
+        #Changes position of snake based off direction
         if direction == 'UP':
             snake_position[1] -= 10
         if direction == 'DOWN':
@@ -200,11 +232,13 @@ def evaluate_fitness(nn, max_steps=10000, no_progress_steps=1000):
 
         snake_body.insert(0, list(snake_position))
 
-        # + 10 to the snakes score if it eats a frit
+        # + 10 to the snakes score if it eats a fruit
         if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
-            score += 10  # High reward for eating fruit
+            score += 100  # High reward for eating fruit
+            fruits_eaten += 1
             fruit_spawn = False
             progress = True
+            max_steps+=1000
         else:
             snake_body.pop()
 
@@ -218,6 +252,7 @@ def evaluate_fitness(nn, max_steps=10000, no_progress_steps=1000):
         if (snake_position[0] < 0 or snake_position[0] >= window_x or
                 snake_position[1] < 0 or snake_position[1] >= window_y):
             score -= 10
+
             break
 
         for block in snake_body[1:]:
@@ -256,6 +291,9 @@ def evaluate_fitness(nn, max_steps=10000, no_progress_steps=1000):
 
       #  if direction_change_count < 3 and steps % 50 == 0:  # Penalize for lack of direction change
           #  score -= 200
+    if index%20 == 0:
+        with open("edittingtextfile.txt", "a") as file:
+            file.write("\nNeural Network Index: "+ str(index) + ", Fruits Eaten: " + str(fruits_eaten) + "\n")
 
     return score + steps/25
 
@@ -265,30 +303,56 @@ def evaluate_fitness(nn, max_steps=10000, no_progress_steps=1000):
 def genetic_algorithm(population, num_generations, initial_mutation_rate):
     mutation_rate = initial_mutation_rate
     for generation in range(num_generations):
-        print(f'Generation {generation + 1}')
+        print("Generation ",generation+1)
+        with open("edittingtextfile.txt", "a") as file:
+            file.write("Generation " + str(generation + 1) + "\n")
 
         # Evaluates each neural network by running it, and then sorting it based off its' score
-        fitness_scores = [evaluate_fitness(nn) for nn in population]
+        fitness_scores = []
+        for i in range(len(population)):
+            fitness = evaluate_fitness(population[i], i)
+            fitness_scores.append(fitness)
+
         max_fitness = max(fitness_scores)
         min_fitness = min(fitness_scores)
-        scaled_fitness = [(score - min_fitness) / (max_fitness - min_fitness + 1e-8) for score in fitness_scores]
+
+
+        #scaled_fitness = [(score - min_fitness) / (max_fitness - min_fitness + 0.00000001) for score in fitness_scores]
+
+        scaled_fitness = []
+        for score in fitness_scores:
+           scaled_fitness.append((score - min_fitness) / (max_fitness - min_fitness + 0.00000001))
+
 
         # Sort the population by scaled fitness scores
-        sorted_population = [x for _, x in sorted(zip(scaled_fitness, population), key=lambda item: item[0], reverse=True)]
+        fitness_cross_population = zip(scaled_fitness, population)
+        sorted_scores = sorted(fitness_cross_population, key = lambda item: item[0], reverse=True)
+
+        sorted_population = []
+        for score in sorted_scores:
+            single_score = score[1]
+            sorted_population.append(single_score)
+
+
+        #sorted_population = [x for _, x in sorted(zip(scaled_fitness, population), key=lambda item: item[0],
+             #                                     reverse=True)]
+
         new_population = sorted_population[:5]  # Keep the top 5 neural networks intact
 
         # Generates the new population using the genetic algorithm
         top_count = 5
+        second_count = 10
         while len(new_population) < population_size:
             for i in range(top_count, min(population_size, top_count + 5)):
                 parent1 = sorted_population[random.randint(0, top_count - 1)]
-                parent2 = sorted_population[random.randint(0, i - 1)]
+               #parent2 = sorted_population[random.randint(0, i - 1)]#
+                parent2 = sorted_population[random.randint(0, second_count - 1)]
                 child = parent1.crossover(parent2)
                 child.mutate(mutation_rate)
                 new_population.append(child)
                 if len(new_population) >= population_size:
                     break
-            top_count += 5
+            #top_count += 5
 
         population = new_population
 
@@ -301,9 +365,11 @@ def genetic_algorithm(population, num_generations, initial_mutation_rate):
         # Visualization of the top neural networks in the current generation
         for index, nn in enumerate(sorted_population[:visualize_count]):
             visualize_snake(nn, generation + 1, index + 1)
-            pygame.display.quit()
+            #pygame.display.quit()
 
     return population
+
+
 
 # Visualization function
 def visualize_snake(nn, generation, nn_index, max_steps=5000):
@@ -322,6 +388,7 @@ def visualize_snake(nn, generation, nn_index, max_steps=5000):
     while steps<max_steps:
         game_window.fill(black)
 
+
         font = pygame.font.SysFont('arial', 24)
         generation_text = font.render(f'Generation: {generation}, NN: {nn_index} , Fruits Eaten: {fruits_eaten}' ,
                                       True, white)
@@ -332,22 +399,22 @@ def visualize_snake(nn, generation, nn_index, max_steps=5000):
 
 
         if prediction == 0:
-            change_to = 'UP'
+            direction_change = 'UP'
         elif prediction == 1:
-            change_to = 'DOWN'
+            direction_change = 'DOWN'
         elif prediction == 2:
-            change_to = 'LEFT'
+            direction_change = 'LEFT'
         else:
-            change_to = 'RIGHT'
+            direction_change = 'RIGHT'
 
         # Prevent snake from reversing direction
-        if change_to == 'UP' and direction != 'DOWN':
+        if direction_change == 'UP' and direction != 'DOWN':
             direction = 'UP'
-        if change_to == 'DOWN' and direction != 'UP':
+        if direction_change == 'DOWN' and direction != 'UP':
             direction = 'DOWN'
-        if change_to == 'LEFT' and direction != 'RIGHT':
+        if direction_change == 'LEFT' and direction != 'RIGHT':
             direction = 'LEFT'
-        if change_to == 'RIGHT' and direction != 'LEFT':
+        if direction_change == 'RIGHT' and direction != 'LEFT':
             direction = 'RIGHT'
 
         # Move the snake in the chosen direction
@@ -366,6 +433,7 @@ def visualize_snake(nn, generation, nn_index, max_steps=5000):
         if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
             fruit_spawn = False
             fruits_eaten+=1
+            max_steps+=1000
         else:
             snake_body.pop()
 
@@ -385,8 +453,9 @@ def visualize_snake(nn, generation, nn_index, max_steps=5000):
                 snake_body[1:].count(snake_position) > 0):
             if (generation+1) % 10==0 and nn_index == 1: # every 10 generations and the best ranked neural network
             # will output to the text file
-                with open("edittingtextfile.txt", "a") as file:
+                with open("experiment2.txt", "a") as file:
                     file.write("Generation "+ str(generation+1)+ ", Fruits Eaten: "+str(fruits_eaten) + "\n")
+
             break
 
         pygame.display.update()
@@ -394,13 +463,13 @@ def visualize_snake(nn, generation, nn_index, max_steps=5000):
 
         steps += 1  # Increment step counter
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
+       # for event in pygame.event.get():
+          #  if event.type == pygame.QUIT:
+          #      pygame.quit()
+            #    return
 
         # Quit Pygame after visualization to free up memory
-    pygame.display.quit()
+  #  pygame.display.quit()
 
 def main():
     population = initialize_population(population_size)
